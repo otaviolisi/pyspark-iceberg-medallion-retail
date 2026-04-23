9ef4443a3a1fcf04e2179fa7514bb76baf2c27dd
otaviolisi <otavio.a200@gmail.com>
2026-04-23 09:37:01 -0300

    full_snapshot_load

--- src/jobs/test_spark.py
+++ src/jobs/test_spark.py
@@ -2,23 +2,6 @@ from src.config.spark_config import create_spark_session
 
 spark = create_spark_session("test-rest-catalog")
 
-spark.sql("CREATE NAMESPACE IF NOT EXISTS demo.bronze")
-
-spark.sql("""
-    CREATE TABLE IF NOT EXISTS demo.bronze.customers (
-        id INT,
-        name STRING,
-        city STRING
-    )
-    USING iceberg
-""")
-
-spark.sql("""
-    INSERT INTO demo.bronze.customers VALUES
-    (1, 'Otavio', 'Ribeirao Preto'),
-    (2, 'Maria', 'Sao Paulo')
-""")
-
-spark.sql("SELECT * FROM demo.bronze.customers").show(truncate=False)
+spark.sql("SELECT * FROM demo.bronze.saleslt_product where ProductID = 680").show(truncate=False)
 
 spark.stop()

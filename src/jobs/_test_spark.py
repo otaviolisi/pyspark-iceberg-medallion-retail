46943451f648d1b25a230cf2f42b8427b3412896
otaviolisi <otavio.a200@gmail.com>
2026-04-27 09:59:50 -0300

    add gitignore

--- src/jobs/_test_spark.py
+++
@@ -1,7 +0,0 @@
-from src.config.spark_config import create_spark_session
-
-spark = create_spark_session("test-rest-catalog")
-
-spark.sql("SELECT * FROM demo.bronze.saleslt_product where ProductID = 680").show(truncate=False)
-
-spark.stop()
